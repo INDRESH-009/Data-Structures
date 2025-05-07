1cc9080a5b5e99b4fa63f42eca75dba9c216ec92
Indresh <indreshmr12345@gmail.com>
2025-05-07 10:20:21 +0530

    folder setup

---
+++ 1.basics/1.prerequisits/1.userIO.cpp
@@ -0,0 +1,25 @@
+//following is the basic structure of all c++ code 
+#include <iostream>
+using namespace std;
+int main(){
+    //program code
+}
+
+/*
+ * The #include is used to imports all the necessay libraries that have inbuilt functions .
+ 1. <iostream> - for user IO 
+ 2. <math.h> - for math operations
+ 3. <string> - for using strings 
+ 4. <bits/stdc++.h> - to inculde most of the standared c++ libraries
+
+ * namespace in C++ is like a container that allows us to group related functions , classes and objects under a unique name. It helps avoid name conflicts in large projects where multiple libraries or modules might define function or variables in the same name
+ std namespace:
+     - The std namespace encloses all the standard library components.
+     - "using namespace std;" is used here to simplify code by allowing direct access 
+       to these components without needing to prefix them with "std::".
+
+
+       
+ */  
+
+
